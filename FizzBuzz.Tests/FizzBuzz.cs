7902dbc95cd4055cef1cdda3b5bff0e51c2cb2a4
oizquierdo <Oizquierdo@voxelgroup.net>
2021-11-19 18:24:04 +0100

    Agregar archivos de proyecto.

---
+++ FizzBuzz.Tests/FizzBuzz.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace FizzBuzz.Tests
+{
+    public class FizzBuzz
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ FizzBuzz/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    public class Class1
+    {
+
+    }
+}
